e1e7694612839a55e9b38b1a1df0200283db0a73
Franklin Dambra <franklindambra@Franklins-MacBook-Pro.local>
2024-05-16 18:00:30 -0500

    fixed layout import

--- pages/error.js
+++ pages/error.js
@@ -1,16 +1,15 @@
 // pages/_error.js
 
 import React from 'react';
-import Layout from '@/layouts/Layout';
+
 
 const ErrorPage = () => {
   return (
-    <Layout>
+
     <div>
       <h1>Error</h1>
       <p>Something went wrong.</p>
     </div>
-    </Layout>
 
   );
 };
