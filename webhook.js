710538d4bc726a306edb4226e60e8162f5802ace
Franklin Dambra <franklindambra@Franklins-MacBook-Pro.local>
2024-06-16 15:51:32 -0500

    str keys

---
+++ webhook.js
@@ -0,0 +1,49 @@
+// Set your secret key. Remember to switch to your live secret key in production.
+// See your keys here: https://dashboard.stripe.com/apikeys
+const stripe = require('stripe')(process.env.NEXT_PUBLIC_TEST_STRIPE_SECRET_KEY);
+
+// Find your endpoint's secret in your Dashboard's webhook settings
+const endpointSecret = 'whsec_...';
+
+// Using Express
+const app = require('express')();
+
+// Use body-parser to retrieve the raw body as a buffer
+const bodyParser = require('body-parser');
+
+const fulfillOrder = (lineItems) => {
+  // TODO: fill me in
+  console.log("Fulfilling order", lineItems);
+}
+
+app.post('/webhook', bodyParser.raw({type: 'application/json'}), async (request, response) => {
+  const payload = request.body;
+  const sig = request.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
+  } catch (err) {
+    return response.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  // Handle the checkout.session.completed event
+  if (event.type === 'checkout.session.completed') {
+    // Retrieve the session. If you require line items in the response, you may include them by expanding line_items.
+    const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
+      event.data.object.id,
+      {
+        expand: ['line_items'],
+      }
+    );
+    const lineItems = sessionWithLineItems.line_items;
+
+    // Fulfill the purchase...
+    fulfillOrder(lineItems);
+  }
+
+  response.status(200).end();
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));
